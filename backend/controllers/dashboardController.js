36b18ab44e143de68c01c3c54d04ed43854daf30
Sabri Jonuzi <sj61243@ubt-uni.net>
2026-07-07 03:49:48 +0200

    Added real dashboard statistics for project managers

--- backend/controllers/dashboardController.js
+++ backend/controllers/dashboardController.js
@@ -3,7 +3,122 @@ const db = require("../config/db");
 const getDashboard = (req, res) => {
   const userId = req.user.id;
 
+
+  // PROJECT MANAGER DASHBOARD
+  if (req.user.role === "project_manager") {
+
+    const dashboardData = {};
+
+
+    const userQuery = `
+      SELECT full_name
+      FROM users
+      WHERE id = ?
+    `;
+
+
+    db.query(userQuery, [userId], (err, userResult) => {
+
+      if (err) {
+        return res.status(500).json(err);
+      }
+
+
+      dashboardData.user = userResult[0];
+
+      const projectsQuery = `
+  SELECT COUNT(DISTINCT p.id) AS totalProjects
+  FROM projects p
+  LEFT JOIN project_members pm
+    ON p.id = pm.project_id
+  WHERE p.created_by = ?
+  OR pm.user_id = ?
+`;
+
+
+     db.query(projectsQuery, [userId, userId], (err, projectsResult) => {
+
+        if (err) {
+          return res.status(500).json(err);
+        }
+
+
+        dashboardData.totalProjects =
+          projectsResult[0].totalProjects;
+
+
+
+        const tasksQuery = `
+  SELECT COUNT(*) AS totalTasks
+  FROM tasks t
+  JOIN projects p
+    ON t.project_id = p.id
+  LEFT JOIN project_members pm
+    ON p.id = pm.project_id
+  WHERE p.created_by = ?
+  OR pm.user_id = ?
+`;
+
+
+       db.query(tasksQuery, [userId, userId], (err, tasksResult) => {
+          if (err) {
+            return res.status(500).json(err);
+          }
+
+
+          dashboardData.totalTasks =
+            tasksResult[0].totalTasks;
+
+
+
+          const completedQuery = `
+  SELECT COUNT(*) AS completedTasks
+  FROM tasks t
+  JOIN projects p
+    ON t.project_id = p.id
+  LEFT JOIN project_members pm
+    ON p.id = pm.project_id
+  WHERE (p.created_by = ?
+  OR pm.user_id = ?)
+  AND t.status = 'done'
+`;
+
+
+         db.query(completedQuery, [userId, userId], (err, completedResult) => {
+
+            if (err) {
+              return res.status(500).json(err);
+            }
+
+
+            dashboardData.completedTasks =
+              completedResult[0].completedTasks;
+
+
+            dashboardData.activeProjects = [];
+
+
+            res.json(dashboardData);
+
+          });
+
+        });
+
+      });
+
+    });
+
+
+    return;
+
+  }
+
+
+
+  // TEAM MEMBER DASHBOARD
   const dashboardData = {};
+
+
 
   const userQuery = `
     SELECT full_name
